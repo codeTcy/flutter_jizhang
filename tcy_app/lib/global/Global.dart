c225e1346bc2109921cf8968602baca7aa0f3398
codeTcy <melocarter@qq.com>
2021-12-08 15:26:18 +0800

    封装dio，封装路由，更改app图标

---
+++ tcy_app/lib/global/Global.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+
+class Global {
+  static Global _instance;
+  Dio dio;
+
+  static Global getInstance() {
+    if (_instance == null) _instance = new Global();
+    return _instance;
+  }
+
+  Global() {
+    dio = new Dio();
+    dio.options = BaseOptions(
+      baseUrl: "https://zxw.td0f7.cn",
+      connectTimeout: 3000,
+      receiveTimeout: 3000,
+      sendTimeout: 3000,
+      headers: {
+        "token": "123",
+      },
+      // contentType: application/x-www-form-urlencoded
+      contentType: Headers.formUrlEncodedContentType,
+      responseType: ResponseType.json,
+    );
+    dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options) {
+        print("请求");
+      },
+      onResponse: (e) {
+        print("返回");
+      },
+      onError: (e) {
+        if (e.type == DioErrorType.CONNECT_TIMEOUT) {
+          print("连接超时错误");
+        } else {
+          print("接口错误");
+        }
+      },
+    ));
+  }
+}
---
+++ tcy_app/lib/routers/router.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tcy_app/view/origin_login_view.dart';
+
+Map<String, WidgetBuilder> routes = {
+  "/": (BuildContext context) => LoginView(), // 首页登录
+};
